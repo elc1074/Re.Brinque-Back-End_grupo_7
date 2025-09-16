9f2bbd0fa76a22876360f8395c9ce835579da8d6
Diovani Stefanello Facco <dsfacco@inf.ufsm.br>
2025-09-15 22:07:29 -0300

    add preset claudinary

--- controllers/cloudinaryController.js
+++ controllers/cloudinaryController.js
@@ -1,14 +1,13 @@
-// controllers/cloudinaryController.js
-
 exports.getCloudinaryConfig = (req, res) => {
   try {
     const cloudinaryConfig = {
       cloudName: process.env.CLOUDINARY_CLOUD_NAME,
       apiKey: process.env.CLOUDINARY_API_KEY,
+      uploadPreset: process.env.CLOUDINARY_UPLOAD_PRESET,
     };
 
-    if (!cloudinaryConfig.cloudName || !cloudinaryConfig.apiKey) {
-      console.error('As variáveis de ambiente do Cloudinary não estão definidas.');
+    if (!cloudinaryConfig.cloudName || !cloudinaryConfig.apiKey || !cloudinaryConfig.uploadPreset) {
+      console.error('As variáveis de ambiente do Cloudinary não estão definidas corretamente.');
       return res.status(500).json({ message: 'Erro de configuração do servidor.' });
     }
 
